dadc416bae3ad9cbfb198ae7950b05d0116e50f1
houdja test <houdja-abdallahpro@hotmail.com>
2022-01-11 14:15:53 +0400

    import du package http + import de dart:convert + modification de la fonction randNUm et randDice + ajout d'un input

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
+import "package:http/http.dart" as http;
 
 void main() {
   return runApp(
@@ -24,51 +27,75 @@ class DicePage extends StatefulWidget {
 class _DicePage extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
-  var randNum = () {
-    return Random().nextInt(6) + 1;
-  };
+
+  get randNum => () {
+        return Random().nextInt(6) + 1;
+      };
 
   void randDice() {
-    setState(
-      () {
-        leftDiceNumber = randNum();
-        rightDiceNumber = randNum();
-      },
-    );
+    leftDiceNumber = randNum();
+    rightDiceNumber = randNum();
   }
+
+  final url = 'https://localhost/testpdo/traitement.php';
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
         children: [
-          Expanded(
-            flex: 1,
+          SizedBox(
+            width: 300,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextButton(
-                onPressed: () {
-                  randDice();
-                },
-                child: Image.asset(
-                  'images/dice$leftDiceNumber.png',
+              padding: EdgeInsets.symmetric(vertical: 30.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  border: UnderlineInputBorder(),
+                  labelText: 'Entrez votre nom',
                 ),
               ),
             ),
           ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextButton(
-                onPressed: () {
-                  randDice();
-                },
-                child: Image.asset(
-                  'images/dice$rightDiceNumber.png',
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          randDice();
+                        },
+                      );
+                    },
+                    child: Image.asset(
+                      'images/dice$leftDiceNumber.png',
+                    ),
+                  ),
                 ),
               ),
-            ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          randDice();
+                        },
+                      );
+                    },
+                    child: Image.asset(
+                      'images/dice$rightDiceNumber.png',
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
