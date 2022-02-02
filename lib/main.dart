83b971f5534ec27be7e2ff6cc3156680501aae7d
houdja test <houdja-abdallahpro@hotmail.com>
2022-02-02 14:12:27 +0400

    connexion API + modification du code

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import "package:http/http.dart" as http;
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -25,77 +26,112 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePage extends State<DicePage> {
+  TextEditingController username = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
 
-  get randNum => () {
-        return Random().nextInt(6) + 1;
-      };
+  sendScore() async {
+    var headers = {'Content-Type': 'application/json'};
+    var request = http.Request(
+        'POST', Uri.parse('http://localhost/apitest/public/api/dedes'));
+    request.body = json.encode(
+        {"pseudo": username.text, "score": leftDiceNumber + rightDiceNumber});
+    request.headers.addAll(headers);
 
-  void randDice() {
-    leftDiceNumber = randNum();
-    rightDiceNumber = randNum();
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
   }
 
-  final url = 'https://localhost/testpdo/traitement.php';
+  void randDice() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            width: 300,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 30.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  focusColor: Colors.white,
-                  border: UnderlineInputBorder(),
-                  labelText: 'Entrez votre nom',
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: TextButton(
-                    onPressed: () {
-                      setState(
-                        () {
-                          randDice();
-                        },
-                      );
-                    },
+          Expanded(
+            flex: 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: Image.asset(
                       'images/dice$leftDiceNumber.png',
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: TextButton(
-                    onPressed: () {
-                      setState(
-                        () {
-                          randDice();
-                        },
-                      );
-                    },
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: Image.asset(
                       'images/dice$rightDiceNumber.png',
                     ),
                   ),
                 ),
+              ],
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Container(
+              color: Colors.white,
+              width: double.infinity,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez remplir les champs';
+                    }
+                    return null;
+                  },
+                  controller: username,
+                  decoration: InputDecoration(
+                    focusColor: Colors.white,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Entrez votre nom',
+                  ),
+                ),
               ),
-            ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black,
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (_formKey.currentState!.validate()) {
+                      randDice();
+                      sendScore();
+                      username.text = '';
+                    }
+                  });
+                },
+                child: Text(
+                  'Lancer les dés'.toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
